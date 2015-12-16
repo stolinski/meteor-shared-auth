09d65fabe181203df6c107e45dbd9d4f258c3f82
stolinski <scotttolinski@gmail.com>
2015-12-15 19:13:24 -0700

    adds production only boolean to desc

--- package.js
+++ package.js
@@ -1,14 +1,15 @@
 Package.describe({
   summary: "Share login among separate domains.",
-  version: "0.0.1",
-  name: "stolinski:shared-auth"
+  version: "0.1.1",
+  name: "stolinski:shared-auth",
+  prodOnly: true
 });
 
 Package.onUse(function (api) {
-  api.versionsFrom('1.0.1');
-  api.use('spacebars');
-  api.use('blaze-html-templates');
-  api.use('kadira:blaze-layout');
+  api.versionsFrom('1.2.1');
+  api.use(['spacebars', 'blaze-html-templates']);
+  api.use(['kadira:blaze-layout@2.3.0']);
+  api.use(['kadira:flow-router@2.10.0']);
   api.add_files([
     'lib/sharedAuthFrame.html',
     'lib/sharedAuthFrame.js',
